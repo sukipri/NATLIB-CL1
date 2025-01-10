88a9a84406adbf4ff61a6dca049b6befd37417e9
sukipri <sukipri9985@gmail.com>
2025-01-11 00:50:56 +0700

    CMT

--- PLSITE/PL_SSRJ_KJ01VIEWKIRIM.php
+++ PLSITE/PL_SSRJ_KJ01VIEWKIRIM.php
@@ -7,17 +7,12 @@
 </div>
 <br>
     <form method="post">
-    <div class="input-group mb-3">
-        <input type="date" class="form-control" required name="txt_tglkj1">
+    <div class="input-group mb-3 mx-2" style="max-width:30rem;">
+        <input type="date" class="form-control" required name="txt_tglkj1" value="<?PHP echo $TG01; ?>">
         <button class="btn btn-info" name="btn_tglkj1">Cari Data</button>
     </div>
     </form>
-<?PHP 
-    if(isset($_POST['btn_tglkj1'])){
 
-    }
-
-?>
     <b>Riwayat Data Terunggah</b>
 <table class="table table-sm table-bordered table-striped">
     <tr class="table-dark">
@@ -32,10 +27,15 @@
         <td>Tgl Periksa</td>
         
     </tr>
-    <?PHP 
-        
+    <?PHP
+        #VARIABLE 
+        $txt_tglkj1 = @$SQL_SL($_POST['txt_tglkj1']);
+
+        if(isset($_POST['btn_tglkj1'])){
+            echo"<META HTTP-EQUIV='Refresh' Content='0; URL=?PG_SA=PL_SSRJ_KJ01VIEWKIRIM&TG01=$txt_tglkj1'>";
+        } 
         #DATA Kunjungan Awal
-        $pl_vrjkj1_sw = $CL_Q($CONN01," $CL_SL nat_rjkj1 WHERE NOT rjkj1_idssen_01='' order by rjkj1_tglmasuk_01 desc");
+        $pl_vrjkj1_sw = $CL_Q($CONN01," $CL_SL nat_rjkj1 WHERE (rjkj1_tglmasuk_01 BETWEEN '$TG01' AND '$TG01') AND  NOT rjkj1_status_01='3' order by rjkj1_tglmasuk_01 desc");
             while($pl_vrjkj1_sww = $CL_FAS($pl_vrjkj1_sw)){
                 #DATA DOKTER
                 $pl_sl_vdkt01_sw = $CL_Q($CONN01,"$SL dokter_idss_01 FROM nat_dokter WHERE dokter_nik_01='$pl_vrjkj1_sww[rjkj1_nikdokter_01]' ");
@@ -85,6 +85,6 @@ $idps_json =  $data2_jsonfor_get['resource']['id'];
     </tr>
     <?PHP } } ?>
 </table>
-               
+
         <!--  -->
             <?PHP }else { echo"<b>Akses Dibatasi </b>"; } ?>

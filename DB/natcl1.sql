935f6712c86e7c537f9277ccf6a7bcc22959e878
sukipri <sukipri9985@gmail.com>
2025-02-08 08:06:29 +0700

    CMT

--- DB/natcl1.sql
+++ DB/natcl1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 30, 2025 at 04:15 AM
+-- Generation Time: Feb 08, 2025 at 01:05 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
